dad9daac6b574fa7d21dea6879d20302a6954b4a
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2022-08-16 19:19:54 +0400

    test_reqests/recive_hash.py

---
+++ src/api/test_reqests/recive_hash.py
@@ -0,0 +1,15 @@
+import requests
+import os, sys
+
+test_url = "http://127.0.0.1:8000/gethash/"
+test_port = 8000
+
+
+
+
+def recive_hash(id):
+    pass
+
+
+if __name__ == '__main__':
+    recive_hash(1)
